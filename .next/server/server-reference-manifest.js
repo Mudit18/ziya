980a9daa8335ab83cd7d3c998bd004bfcb0c3a0b
Mudit Dangi <6501425+Mudit18@users.noreply.github.com>
2024-01-27 16:49:10 +0530

    Fixed the metadata

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"sNPLUqVq6V6GEYPHVUArxGfq0JNbHQs7oIXJpsBTbnY=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"BsNL2NH2aU4dA+JVRiLazZN0KFGY1vS2scRs8Uxln0s=\"}"
